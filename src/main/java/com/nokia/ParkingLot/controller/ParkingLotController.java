b3c868d57484559aee10c613ec322c058d8b3bf6
Bhuvan Parashar <bparashar@aripratech.com>
2021-02-08 02:52:41 +0530

    Added instuctions for deployment -Added API to add slots

--- src/main/java/com/nokia/ParkingLot/controller/ParkingLotController.java
+++ src/main/java/com/nokia/ParkingLot/controller/ParkingLotController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nokia.ParkingLot.KafkaConfigs.KafkaProducer;
 import com.nokia.ParkingLot.Model.Car;
+import com.nokia.ParkingLot.Model.ParkingSlot;
 import com.nokia.ParkingLot.service.CarSlotAllocationService;
 import com.nokia.ParkingLot.service.ParkingLotService;
 
@@ -31,8 +32,6 @@ public class ParkingLotController {
 	@PostMapping("/entry")
 	@ResponseBody
 	public String entryInParkingLot(@RequestBody Car car) {
-		System.out.println("Entry");
-		System.out.println(car.toString());
 		if (!carSlotAllocationService.checkForCar(car)) {
 			if (carSlotAllocationService.checkForFreeSLot()) {
 				producer.parkTheCar(car);
@@ -41,22 +40,16 @@ public class ParkingLotController {
 				return "No Slots available.";
 		} else
 			return "Car " + car.getName() + " is already parked.";
-
 	}
 
 	@GetMapping("/slots")
-	public Iterable<com.nokia.ParkingLot.Model.ParkingSlot> ParkingSlot() {
-		// TODO Auto-generated method stub
-		System.out.println("Slots");
+	public Iterable<ParkingSlot> ParkingSlot() {
 		return parkingLotService.getParkingSlots();
 	}
 
 	@PostMapping("/exit")
 	@ResponseBody
 	public String exitFromParkingLot(@RequestBody Car car) {
-		// TODO Auto-generated method stub
-		System.out.println("Exit");
-		System.out.println(car.toString());
 		if (carSlotAllocationService.checkForCar(car)) {
 			producer.billTheCar(car);
 			return "Car " + car.getName() + " billed and leaving parking slot.";
@@ -66,8 +59,12 @@ public class ParkingLotController {
 
 	@GetMapping("/available-slots")
 	public Object availableSlots() {
-		// TODO Auto-generated method stub
-		System.out.println("Available Slots");
 		return parkingLotService.getAvailabeParkingSlots();
+	}
+	
+	@PostMapping("/addSlot")
+	@ResponseBody
+	public void createParkingSlot(@RequestBody ParkingSlot parkingSlot) {
+		parkingLotService.saveParkingSlot(parkingSlot);
 	}
 }
